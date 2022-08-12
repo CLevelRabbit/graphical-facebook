a0556296a192e959c1c3633d8859d38721d84f26
CLevelRabbit <therabbitholeil22@gmail.com>
2022-08-12 12:24:09 +0300

    added command line param

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Sorts = popularity release_date title
+import sys
 import os
 import pandas as pd
 import requests
@@ -31,7 +32,7 @@ REACTION_FIELDS = [
 IMAGES_POST_PATH = './images/{}'
 COOKIE_FILE = 'facebook.com_cookies.txt'
 PAGE_NAME = 'hapshuta'
-DB_PATH = f'{PAGE_NAME}_posts.csv'
+DB_PATH_FMT = '{}_posts.csv'
 
 
 def download_images(post):
@@ -48,8 +49,15 @@ def download_images(post):
 
 
 def main():
+    if len(sys.argv) == 2:
+        page_name = sys.argv[1]
+    else:
+        page_name = PAGE_NAME
+
+    db_path = DB_PATH_FMT.format(page_name)
+
     # Find out if first write or not
-    if os.path.exists(DB_PATH):
+    if os.path.exists(db_path):
         write_kwargs = {'mode': 'a', 'index': False, 'header': False}
     else:
         write_kwargs = {'index': False}
@@ -58,7 +66,7 @@ def main():
 
     # You can set "start_url=<>" to the page url you wish to start from.
     for i, post in enumerate(
-            get_posts(PAGE_NAME, page_limit=200000000, cookies=COOKIE_FILE, extra_info=True)):
+            get_posts(page_name, page_limit=200000000, cookies=COOKIE_FILE, extra_info=True)):
         post_dict = {key: post.get(key, default) for key, default in FIELDS}
         reactions = post.get('reactions', {})
         if not reactions:
@@ -76,7 +84,7 @@ def main():
         print('Saving post! %d' % i)
         print(post_dict)
         df = pd.json_normalize([post_dict])
-        df.to_csv(DB_PATH, **write_kwargs)
+        df.to_csv(db_path, **write_kwargs)
         write_kwargs = {'mode': 'a', 'index': False, 'header': False}
 
 
